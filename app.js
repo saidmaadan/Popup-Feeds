10380e18568975eff8ca3fb813edcb5b5d1c3739
Said Maadan <saidmaadan@users.noreply.github.com>
2015-04-15 19:45:44 -0500

    Add services and ui-router

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 (function(){
 	"use strict"
 
-	var app = angular.module("PopUp",[]);
+	var app = angular.module("PopUp",['ui-router']);
 
-	app.controller("MainController", MainController);
+	app.config(['$stateProvider', '$urlRouteProvider',
+		function($stateProvider, $urlRouteProvider){
+			$stateProvider
+				.state('home',{
+					url: '/home',
+					templateUrl: 'home.html',
+					controller: 'MainControllerontroller'
+				});
 
-	function MainController($scope){
-		$scope.posts = [
+				$urlRouteProvider.otherwise('home')
+		}])
+
+	app.factory('posts', [function(){
+		var o = {
+			posts: [
 		{title: "post 1", upvotes: 4},
 		{title: "post 2", upvotes: 3},
 		{title: "post 3", upvotes: 1},
 		{title: "post 4", upvotes: 7},
 		{title: "post 5", upvotes: 9},
 		{title: "post 6", upvotes: 2}
-		];
+		]
+		};
+		return o;
+	}])
+
+	app.controller("MainController", MainController);
+
+	function MainController($scope, posts){
+		$scope.posts = posts.posts
 
 		$scope.addPost =function(){
 			if(!$scope.title || $scope.title === ''){
